2f92d08f4cce45d3a7da5385a040cae19a13ede7
EmanueleIppolito <emanuele2410@gmail.com>
2026-02-20 17:51:18 +0100

    Milestone 3 fatta

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,24 @@ function App() {
   const [count, setCount] = useState(0)
   const [addedProducts, setAddedProducts] = useState([])
 
+
   const addToCart = (product) => {
     const existingProduct = addedProducts.find((item => item.name === product.name))
     if(!existingProduct){
       setAddedProducts([...addedProducts, {...product, quantity: 1}])
-    }
+    } else {
+      setAddedProducts(
+      addedProducts.map((item) => 
+      item.name === product.name
+  ? {...item, quantity: item.quantity + 1}
+  : item))}
   }
-
+  const removeFromCart = (productName) => {
+  setAddedProducts(
+    addedProducts.filter((item) => item.name !== productName)
+  )
+}
+  const total = addedProducts.reduce((acc, item) => acc + item.price * item.quantity, 0)
   return (
     <>
       <div>
@@ -31,8 +42,11 @@ function App() {
         <>
         <h2>Carrello</h2>
         <ul>
-          {addedProducts.map((product) => (<li key= {product.name}>{product.name}, Numero articoli: <strong>{product.quantity}</strong></li>))}
+          {addedProducts.map((item) =>
+           (<li key= {item.name}>{item.name}, Numero articoli: <strong>{item.quantity}</strong> € {(item.price * item.quantity).toFixed(2)}<button onClick={() => removeFromCart(item.name)}> Elimina dal Carrello </button></li>))}
         </ul>
+        <h2>Totale:</h2>
+        <p><strong>€{total.toFixed(2)}</strong></p>
         </>
         }
       </div>
